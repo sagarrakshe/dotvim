4ea18fc8250d216dcb0faa1798a8b62b0a128fe8
sagarrakshe <sagarrakshe2@gmail.com>
2013-10-26 13:50:21 +0530

    updated install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,13 @@
 # Distributed under terms of the GNU GPL license.
 
 mv ~/.vimrc ~/.vimrc_bak &> /dev/null
+mv ~/.gvimrc ~/.gvimrc_bak &> /dev/null
 
-ln -s vimrc ~/.vimrc
-ln -s gvimrc ~/.gvimrc
+ln -s ~/.vim/vimrc ~/.vimrc
+ln -s ~/.vim/gvimrc ~/.gvimrc
+
+echo -e "Installing plugin...\n"
+git submodule init
+git submodule update
+echo "Done. Ready to use vim!"
+
